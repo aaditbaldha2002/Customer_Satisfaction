e9bc44ec0059682c4e566c689eb8c798e5aabe12
Aadit Harshal Baldha <aaditbaldha2002@gmail.com>
2025-07-08 20:53:26 -0400

    changes to run_pipeline.py

--- run_pipeline.py
+++ run_pipeline.py
@@ -4,3 +4,4 @@ from pipelines.training_pipeline import training_pipeline
 if __name__ == "__main__":
     print(Client().active_stack.experiment_tracker.get_tracking_uri())
     pipeline_instance = training_pipeline(data_path="dataset/olist_customers_dataset.csv")
+
